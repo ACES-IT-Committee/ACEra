5620ed7911d1099b65f33fa3a07317deedcabbc0
Mohammed159159 <mohammedhany300@gmail.com>
2023-04-08 23:34:22 +0200

    dev: Add animations

--- frontend_next/src/containers/ChatArea/ChatArea.tsx
+++ frontend_next/src/containers/ChatArea/ChatArea.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import style from "./ChatArea.module.scss";
 import msgs from "@/constants/messages";
+import {motion} from 'framer-motion'
 import { Chatbox, Message } from "@/components";
 import images from "@/constants/images";
 const ChatArea = () => {
@@ -10,7 +11,8 @@ const ChatArea = () => {
         <div className={style["app__chat_area"]}>
             <img src={images.aces.src} />
             {msgs.map((message, index) =>
-                <Message key={index} message={message} />)}
+                    <Message key={index} message={message} />
+                )}
             <Chatbox messages={messages} postMessage={setMessages} />
         </div>
     );
